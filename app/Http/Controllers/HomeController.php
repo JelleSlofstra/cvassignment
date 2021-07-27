af6189583757b8094a9e5216655c081da25d6c9c
JelleSlofstra <jelleslofstra@gmail.com>
2021-07-27 14:32:08 +0200

    added all the db tables and views for them

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,9 +12,27 @@ class HomeController {
     public function index()
     {
         $statement = MySql::query('select * from users');
+        $userinfo = $statement->fetchAll(PDO::FETCH_OBJ);
 
-        $results = $statement->fetchAll(PDO::FETCH_OBJ);
+        $statement = MySql::query('select * FROM `jobs` WHERE user_id = 1');
+        $jobs = $statement->fetchAll(PDO::FETCH_OBJ);
 
-        return View::render('home.view', $results);
+        $statement = MySql::query('select * FROM `educations` WHERE user_id = 1');
+        $educations = $statement->fetchAll(PDO::FETCH_OBJ);
+
+        $statement = MySql::query('select * FROM `volunteerjobs` WHERE user_id = 1');
+        $volunteerjobs = $statement->fetchAll(PDO::FETCH_OBJ);
+
+        $statement = MySql::query('select * FROM `skills` WHERE user_id = 1');
+        $skills = $statement->fetchAll(PDO::FETCH_OBJ);
+
+        $statement = MySql::query('select * FROM `hobbies` WHERE user_id = 1');
+        $hobbies = $statement->fetchAll(PDO::FETCH_OBJ);
+
+        $cv = [$userinfo, $jobs, $educations, $volunteerjobs, $skills, $hobbies];
+
+        //dd($cv);
+
+        return View::render('home.view', $cv);
     }
 }
--- views/home.view.php
+++ views/home.view.php
@@ -2,15 +2,85 @@
 
 <div class="main">
     <p>
-        <div> Name: <?= $vars[0]->first_name . ' ' . $vars[0]->last_name ?> </div>
-        <div> E-mail: <?= $vars[0]->email ?> </div>
-        <div> Phone: <?= $vars[0]->phone ?> </div>    
-        <div> Date of birth: <?= $vars[0]->date_birth ?> </div>    
+        <div> Naam: <?= $vars[0][0]->first_name . ' ' . $vars[0][0]->last_name ?> </div>
+        <div> E-mail: <?= $vars[0][0]->email ?> </div>
+        <div> Telefoonnummer: <?= $vars[0][0]->phone ?> </div>    
+        <div> Geboortedatum: <?= $vars[0][0]->date_birth ?> </div>    
     </p>
 
     <p>
-        <?= $vars[0]->pitch ?>  
+        <?= $vars[0][0]->pitch ?>  
     </p>
+
+    <p>
+        Werkervaring
+        <ul>            
+            <?php foreach($vars[1] as $job): ?>
+                <li>
+                    <?= $job->start_year ?> -
+                    <?php if (!$job->end_year): ?>
+                        heden:
+                    <?php else: ?>
+                        <?= $job->end_year ?>:
+                    <?php endif; ?>
+                    <b> <?= $job->function ?></b>
+                    <?= $job->job_info ?>
+                </li>            
+            <?php endforeach; ?>
+        </ul>
+    </p>
+
+    <p>
+        Opleidingen
+        <ul>            
+            <?php foreach($vars[2] as $education): ?>
+                <li>
+                    <?= $education->start_year ?> -
+                    <?php if (!$education->end_year): ?>
+                        heden:
+                    <?php else: ?>
+                        <?= $education->end_year ?>:
+                    <?php endif; ?>
+                    <b> <?= $education->education_name ?></b>
+                    <?= $education->education_info ?>
+                </li>            
+            <?php endforeach; ?>
+        </ul>
+    </p> 
+
+    <p>
+        Vrijwilligerswerk
+        <ul>            
+            <?php foreach($vars[3] as $volunteerjob): ?>
+                <li>
+                    <?= $volunteerjob->start_year ?> -
+                    <?php if (!$volunteerjob->end_year): ?>
+                        heden:
+                    <?php else: ?>
+                        <?= $volunteerjob->end_year ?>:
+                    <?php endif; ?>
+                    <b> <?= $volunteerjob->function ?></b>
+                    <?= $volunteerjob->volunteerjob_info ?>
+                </li>            
+            <?php endforeach; ?>
+        </ul>
+    </p>
+
+    <p>
+        Skills:           
+            <?php foreach($vars[4] as $skill): ?>
+                <?= $skill->name ?>, 
+            <?php endforeach; ?>
+    </p>
+
+    <p>
+        Interesses:           
+            <?php foreach($vars[5] as $hobby): ?>
+                <?= $hobby->name ?>, 
+            <?php endforeach; ?>
+    </p>
+
+    
 </div>
 
 <?php require 'views/partials/footer.view.php' ?>
